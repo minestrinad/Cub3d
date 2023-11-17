87376fc8776691a17241e23bcf328f98b1884a15
Neri Cortigiani <newi.corty@gmail.com>
2023-11-17 16:08:53 +0100

    bo

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: everonel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ncortigi <ncortigi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 15:44:54 by everonel          #+#    #+#             */
-/*   Updated: 2022/10/22 16:12:34 by everonel         ###   ########.fr       */
+/*   Updated: 2023/11/17 16:06:23 by ncortigi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "libft.h"
 
+#include "libft.h"
+#include "stdio.h"
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	int		start;
@@ -23,7 +24,9 @@ char	*ft_strtrim(char const *s1, char const *set)
 		start++;
 	while (ft_strchr(set, s1[end]) && end >= start)
 		end--;
+	printf("s1:[%s]\n", s1);
 	new = ft_substr(s1, start, (end + 1 - start));
+	printf("new:[%s]\n", new);
 	if (!new)
 		return (NULL);
 	return (new);
